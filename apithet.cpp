75481a92c299136322ca7dd3582e49468214b3d5
gulpfriction <mail.gulpfriction@gmail.com>
2016-11-28 15:44:34 +0530

    renaming files with name MainWindow

---
+++ apithet.cpp
@@ -0,0 +1,73 @@
+/*
+ *  Copyright [2016] [mail.gulpfriction@gmail.com]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "apithet.h"
+#include "ui_apithet.h"
+
+APIthet::APIthet(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    manager = new QNetworkAccessManager(this);
+    header = new httpHeaders;
+
+    //proxy = new QNetworkProxy;
+
+    //this->setWindowIcon(QIcon(":/icons/APIthet_icon.png"));
+
+    //Set all defaults
+    ui->lineEditHeader->setStyleSheet("color: blue");
+    ui->lineEditHeaderVal->setStyleSheet("color: blue");
+    ui->plainTextEditPayload->setStyleSheet("color: brown");
+
+    ui->lineEditURL->setPlaceholderText("Target URL");
+    ui->lineEditRandParam->setPlaceholderText("Randomize JSON");
+    //ui->lineEditHeader->setPlaceholderText("Header");
+    //ui->lineEditHeader->setPlaceholderText("Header Value");
+
+    ui->lineEditUsername->setPlaceholderText("Username");
+    ui->lineEditPassword->setPlaceholderText("Password");
+
+    ui->lineEditPassword->setEchoMode(QLineEdit::Password);
+
+    ui->tableWidget->setColumnCount(4);
+    ui->tableWidget->setRowCount(10);
+
+    setMethods();
+    setEncoding();
+    setRandTypes();
+
+    connect(ui->comboBoxHTTPMethods, SIGNAL(activated(QString)), this, SLOT(comboBoxHTTPSlot(QString)));
+    connect(ui->pushButtonClear, SIGNAL(clicked()), this, SLOT(pushButtonClearSlot()));
+    connect(ui->pushButtonAddHeader, SIGNAL(clicked()), this, SLOT(textBrowserHeaderSlot()));
+    //connect(ui->pushButtonAddPayload, SIGNAL(clicked()), this, SLOT(textBrowserPayloadSlot()));
+    connect(ui->plainTextEditPayload, SIGNAL(textChanged()), this, SLOT(pushButtonEncodeSlot()));
+    connect(ui->plainTextEditPayload, SIGNAL(textChanged()), this, SLOT(getRandJsonParam()));
+    //connect(ui->pushButtonEncode, SIGNAL(clicked()), this, SLOT(textBrowserEncodeSlot()));
+    connect(ui->lineEditHeader, SIGNAL(textChanged(QString)), this, SLOT(pushButtonHeaderSlot()));
+    connect(ui->lineEditRandParam, SIGNAL(textChanged(QString)), this, SLOT(comboBoxRandParamSlot()));
+    connect(ui->pushButtonRun, SIGNAL(clicked()), this, SLOT(pushButtonRunSlot()));
+    connect(ui->checkBoxProxy, SIGNAL(stateChanged(int)), this, SLOT(enableProxySlot(int)));
+    connect(ui->checkBoxAuth, SIGNAL(stateChanged(int)), this, SLOT(enableAuthSlot(int)));
+    connect(manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(replyFinished(QNetworkReply*)));
+    connect(manager, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+
+    //eventLoop.exec();
+}
+
+APIthet::~APIthet()
+{
+    delete ui;
+}
---
+++ apithet.h
@@ -0,0 +1,342 @@
+/*
+ *  Copyright [2016] [mail.gulpfriction@gmail.com]
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#ifndef APITHET_H
+#define APITHET_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QSplashScreen>
+
+#include <QNetworkAccessManager>
+#include <QNetworkRequest>
+#include <QNetworkReply>
+#include <QNetworkProxy>
+
+#include <QThread>
+#include <QEventLoop>
+
+#include <QLinkedList>
+#include <QList>
+#include <QPair>
+#include <QHash>
+#include <iterator>
+
+#include <QUrl>
+#include <QUrlQuery>
+
+#include <QStringList>
+
+#include <QJsonDocument>
+#include <QJsonValue>
+#include <QJsonObject>
+#include <QJsonArray>
+
+
+#define APPICON ":/icons/APIthet_icon.png"
+
+#define CONTENT_HEADER  "Content-Security-Policy"
+#define XFRAME_HEADER   "X-Frame-Options"
+#define XCONTENT_HEADER "X-Content-Type-Options"
+#define HSTS_HEADER     "Strict-Transport-Security"
+#define XSS_HEADER      "X-XSS-Protection"
+#define CSRF_TOKEN_1    "X-Csrf-Token"
+#define CSRF_TOKEN_2    "X-CSRFToken"
+#define CSRF_TOKEN_3    "X-XSRF-TOKEN"
+
+#define ALLOW_ORIGIN    "Access-Control-Allow-Origin"
+#define ALLOW_CRED      "Access-Control-Allow-Credentials"
+#define ALLOW_METHOD    "Access-Control-Allow-Methods"
+
+#define maliciousPart1  "\"><ScRiPt SRC="
+#define maliciousPart2  "></ScRiPt>"
+#define htmlInjPayload  "<html>APIthet is testing your APIs</html>"
+#define htmlInjContent  "APIthet is testing your APIs"
+#define redirectQuery   "redirect=http://www.example.com"
+#define sqlUrlInjecionPayload  "35577585' or 9964=9964--"
+
+#define redirectDomain  "www.example.com"
+
+#define RANDOM_CHAR_LEN 5
+#define RANDOM_NAME_LEN 6
+#define SINGLE_CHAR_LEN 1
+#define RANDOM_DOMAIN_LEN   6
+#define RANDOM_PHONE_NUM_LEN    10
+#define RANDOM_SITE_TYPE_LEN    3
+
+namespace Ui {
+class MainWindow;
+}
+
+enum httpMethods {
+    GET = 0,
+    POST,
+    PUT,
+    HEAD,
+    DELETE,
+    TRACE,
+    CONNECT,
+    OPTIONS,
+};
+
+enum attackTypes {
+    NO_ATTACK = 0,
+    XSS,
+    SQL_INJ,
+    CSRF,
+    HTML_INJ,
+    OPEN_REDIRECT,
+    HTTP_SPLITTING,
+    CRLF,
+    CORS,
+    HEADER_FUZZ,
+};
+
+enum sqliTypes {
+    AUTH_BYPASS = 0,
+    DATA_LEAKAGE,
+    DB_FINGERPRINTING,
+};
+
+enum randPayloadTypes {
+    NONE = 0,
+    NAME,
+    NAME_SNAME,
+    EMAIL,
+    WEBSITE,
+    IP_ADDR,
+    PHONE_NUMBER,
+};
+
+class apiList {
+private:
+    //Target Url
+    QUrl targetUrl;
+    //HTTP Request
+    QNetworkRequest reqHandle;
+    //HTTP Reply
+    QNetworkReply *replyHandle;
+    //Network Access Manager
+    QNetworkAccessManager networkAccessManager;
+
+    ~apiList();
+};
+
+class httpHeaders {
+public:
+    httpHeaders();
+    void incrementHeaders();
+    void decrementHeaders();
+    void insertHeader(QString hdrStr , QString hdrValStr);
+    bool hasElements();
+    bool isEmpty();
+    QString getLastHeader();
+    QString getLastHeaderVal();
+    void clearList();
+    uint getHeaderCount();
+
+private:
+    uint numHeaders;
+    QLinkedList<QString> header;
+    QLinkedList<QString> headerVal;
+};
+
+
+class APIthet : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit APIthet(QWidget *parent = 0);
+    void setMethods();
+    void setEncoding();
+    void setRandTypes();
+    //void setProxy();
+
+    apiList *apiEntry;
+    QNetworkProxy proxy;
+
+    ~APIthet();
+
+public slots:
+
+    //slot to handle cleaar button
+    void pushButtonClearSlot();
+
+    //slot for adding header
+    void textBrowserHeaderSlot();
+
+    //slot for adding payload
+    void textBrowserPayloadSlot();
+
+    //slot for encoding payload
+    void textBrowserEncodeSlot();
+
+    //slot to enable/disable Encode button
+    void pushButtonEncodeSlot();
+
+    // Slot to enable/disable Header button
+    void pushButtonHeaderSlot();
+
+    //Slot to handle Run button
+    void pushButtonRunSlot();
+
+    //slot to handle http request
+    void replyFinished (QNetworkReply *reply);
+
+    //slot to handle enable proxy button
+    void enableProxySlot(int state);
+
+    //slot to handle enable auth button
+    void enableAuthSlot(int state);
+
+    //Slot to handle payload text with selected HTTP method
+    void comboBoxHTTPSlot(QString selectedMethod);
+
+    //slot for the payload line edit
+    void getRandJsonParam();
+
+    //slot for random payload combobox
+    void comboBoxRandParamSlot();
+
+private:
+    Ui::MainWindow *ui;
+
+    //Handle error conditions
+    //to check url text
+    bool lineUrlValid();
+    void processHttpReq();
+    void sendGEThttp();
+    void sendPOSThttp();
+    void sendPUThttp();
+    void setHttpHeaders(QNetworkRequest *httpRequest);
+    void checkSecHeaders(QNetworkReply *reply);
+
+    void genKeyValueMap(QUrl *targetURL);
+    void genRandomStr(QString *randStr, int strLen);
+    void genRandomNumericalStr(QString *randStr, int strLen);
+
+    void genMaliciousStr(QString *maliciousStr, QString keyVal);
+    void reportMalPayload(QString Payload);
+    void analyzePayload(QString replyPayload);
+    bool genJSONpayload(QString replyPayload);
+    void analyzeJsonList();
+    void analyzeJsonList(QList<QVariant> *localList);
+    void analyzeJsonMap();
+    void analyzeJsonNode(QVariant *jsonNode);
+
+    void setApplicationProxy();
+
+    void populateJsonParamMap(QString key, QString value);
+    void writeJsonParamMap(QString jsonStrings);
+    void analyzeJsonMap(QMap<QString, QVariant> *localMap);
+
+    void insertInvertedCommas(QString *keyVal);
+    void genJSonMaliciousParam(QString *maliciousJsonParam, QString keyValue);
+
+    void genRandomPhone(QString *);
+    void genRandomIP(QString *);
+    void genRandomWebsite(QString *);
+    void genRandomEmail(QString *);
+    void genRandNameSname(QString *);
+    void genRandNames(QString *);
+    QString *genRandSqliPayload(sqliTypes injType);
+
+    void setRandParamString();
+    void setRandString(QString *randParamString);
+
+    void prepareCsrfRequest(QNetworkRequest *httpRequest);
+    void showHeaderResult();
+
+    //attack functions
+    void performUrlXSS(QNetworkRequest *httpRequest);
+
+    void performJsonXSS(QNetworkRequest *httpRequest);
+
+    void performGetCSRF(QNetworkRequest *httpRequest);
+
+    void performPostCSRF(QNetworkRequest *httpRequest);
+
+    void performHtmlInjection(QNetworkRequest *httpRequest);
+
+    void performOpenRedirect(QNetworkRequest *httpRequest);
+
+    void performUrlSQLI(QNetworkRequest *httpRequest);
+
+    //process reply after attack
+    void processCsrfReply(QNetworkReply *reply);
+
+    void processXssReply(QNetworkReply *reply);
+
+    void processHtmlInjectionReply(QNetworkReply *reply);
+
+    void processOpenRedirectReply(QNetworkReply *reply);
+
+    void processSqliReply(QNetworkReply *reply);
+
+    void analyzeApplicationFootprint(QNetworkReply *reply);
+
+    void setDefault();
+
+    QNetworkAccessManager *manager;
+
+    QList<QPair<QString, QString> > urlParamMap;
+    QList<QPair<QString, QString> >::iterator mapEntry;
+
+    QHash<QString, QString> keyLookupTable;
+
+    QMap<QString, QVariant> jsonMap;
+
+    QMap<QString, quint16> jsonParamsMap;
+
+    QList<QVariant> jsonList;
+
+    httpMethods methods;
+    randPayloadTypes randParamType;
+    attackTypes attackType;
+
+    QEventLoop eventLoop;
+
+    httpHeaders *header;
+
+    //flag to specify a payload crafted CSRF
+    //bool csrfPayload;
+    bool csrfIssueLikely;
+    //there was and authentication header in the request
+    bool headerHasAuth;
+    //the request has queries in URL or body
+    bool hasQueryParams;
+
+    bool postQueryHasBody;
+
+    bool likelyUnauth;
+    //password likely present in query
+    bool passwdInGetQuery;
+
+    bool passwdInPostQuery;
+
+    int contentHeaderMissed;
+    int xFrameHeaderMissed;
+    int xContentHeaderMissed;
+    int hstsHeaderMissed;
+    int xssProtHeaderMissed;
+    int csrfHeaderMissed;
+
+    QString currentParam;
+};
+
+
+#endif // APITHET_H
